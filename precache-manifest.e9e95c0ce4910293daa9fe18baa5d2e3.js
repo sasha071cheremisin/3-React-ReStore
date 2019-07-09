de9449e1e243ce9bb3b58a427de5a207399d98b4
alex <sasha071cheremisin@mail.ru>
2019-07-09 17:07:20 +0300

    Updates

---
+++ precache-manifest.e9e95c0ce4910293daa9fe18baa5d2e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb007b1fa7780f145aba7d038497a5c7",
+    "url": "/3-React-ReStore/index.html"
+  },
+  {
+    "revision": "21ba637f0b817bcf4179",
+    "url": "/3-React-ReStore/static/css/main.6784032b.chunk.css"
+  },
+  {
+    "revision": "e73a8ac848a3c487cefd",
+    "url": "/3-React-ReStore/static/js/2.9a625231.chunk.js"
+  },
+  {
+    "revision": "21ba637f0b817bcf4179",
+    "url": "/3-React-ReStore/static/js/main.f58aa9ef.chunk.js"
+  },
+  {
+    "revision": "1c6ff9ad3386bd60d162",
+    "url": "/3-React-ReStore/static/js/runtime~main.46caeb80.js"
+  }
+]);
